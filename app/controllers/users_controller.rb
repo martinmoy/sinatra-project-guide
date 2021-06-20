0cdd782d2e9c14d8909b93bb26bf8358fd333c5e
Martin Moy <martinmoy1991@gmail.com>
2021-06-20 18:44:52 -0400

    mount user_controller, created view login.erb and signup.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
 
+    # render the login in page
+    get '/login' do 
+        erb :login
+    end
 
-    
+    get '/signup' do 
+        erb :signup
+    end
+
+
+
+
 end
