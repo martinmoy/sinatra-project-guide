3306d1d3c111ecc2fc7f302e6f247cca08fec4b9
Martin Moy <martinmoy1991@gmail.com>
2021-06-20 17:06:19 -0400

    associtation between users and countries created

--- db/migrate/20210620205532_create_countries.rb
+++ db/migrate/20210620205532_create_countries.rb
@@ -2,6 +2,7 @@ class CreateCountries < ActiveRecord::Migration
   def change
     create_table :countries do |t|
       t.string :name
+      t.integer :user_id
       t.timestamps null: false
     end
   end
