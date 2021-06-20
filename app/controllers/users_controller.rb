920eb7a98abaab364eace3474997bf353576ac57
Martin Moy <martinmoy1991@gmail.com>
2021-06-20 19:59:06 -0400

    authenticate for session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,20 @@ class UsersController < ApplicationController
         erb :login
     end
 
+    post '/login' do
+        #find the user
+        @user = User.find_by(email: params[:email])
+        #authenticate the users
+        if @user.authenticate(params[:password])
+            redirect "/success"
+        else 
+            redirect "/failure"
+        end
+        
+        #log in the user 
+        # redirect the user to another page
+    end
+
     get '/signup' do 
         erb :signup
     end
